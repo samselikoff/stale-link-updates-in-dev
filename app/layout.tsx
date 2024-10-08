156d65f803455adfb2653aa50ee84d4db562dc9d
Sam Selikoff <sam.selikoff@gmail.com>
2024-10-08 15:24:02 -0400

    Minimal reproduction

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,22 @@
+import Link from 'next/link';
+
 export default function RootLayout({ children }) {
   return (
     <html>
       <head />
-      <body>{children}</body>
+      <body>
+        <nav style={{ display: 'flex', gap: '10px' }}>
+          <Link href="/">Home</Link>
+
+          {Array.from(Array(10).keys()).map((i) => (
+            <Link href={`/post/${i}`} key={i}>
+              Post {i}
+            </Link>
+          ))}
+        </nav>
+
+        {children}
+      </body>
     </html>
   );
 }
